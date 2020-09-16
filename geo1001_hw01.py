cd58a24b1f3cb8838138bb34a4a8db6649a90e75
Pratyush <pratyushkmr13@gmail.com>
2020-09-16 14:21:58 +0200

    added the mean var std discriptions

--- geo1001_hw01.py
+++ geo1001_hw01.py
@@ -6,10 +6,10 @@
 import os
 import pandas as pd
 import datetime
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 #%% # Functions
-def megadf(path='./GEO1001_maths/Assignment/asg1_21sep/hw01'):
+def megadf(path='./hw01'):
     """takes the path of existing sensor datasets and
         returns a dataframe with all datasets combined
 
@@ -42,7 +42,7 @@ df = megadf()
 # %%
 # to continue with the assignment
 """
-df=pd.read_json('./GEO1001_maths/Assignment/asg1_21sep/joinedDF.json')
+df=pd.read_json('./joinedDF.json')
 df['FORMATTED DATE-TIME']=df['FORMATTED DATE-TIME'].apply(lambda d: datetime.datetime.fromtimestamp(int(d)/1000).strftime('%Y-%m-%d %H:%M:%S'))
 #df['FORMATTED DATE-TIME'] = pd.to_datetime(df['FORMATTED DATE-TIME'] , unit='ns')#.astype('datetime64[ns]')
 """
@@ -59,10 +59,22 @@ overlap in different colors with a legend.
 Generate 3 plots that include the 5 sensors boxplot for: 
 Wind Speed, Wind Direction and Temperature.
 """
+# %%
+#from part a1
+# ## mean stats: mean, variance and standard deviation
+collist=  ['FORMATTED DATE-TIME', 'Direction ‚ True', 'Wind Speed',
+       'Crosswind Speed', 'Headwind Speed', 'Temperature', 'Globe Temperature',
+       'Wind Chill', 'Relative Humidity', 'Heat Stress Index', 'Dew Point',
+       'Psychro Wet Bulb Temperature', 'Station Pressure',
+       'Barometric Pressure', 'Altitude', 'Density Altitude',
+       'NA Wet Bulb Temperature', 'WBGT', 'TWL', 'Direction ‚ Mag', 'sensor']
 
-g = sns.FacetGrid(df, col='sensor')
-g.map(sns.distplot(bin=5), "Temperature")
-plt.show()
 
 # %%
-#les see how it goes on in github
+print('mean'
+print(df.groupby('sensor').mean())
+print('var')
+print(df.groupby('sensor').var())
+print('std')
+print(df.groupby('sensor').std())
+#%%
